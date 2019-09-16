9057023a7d20abd2107c9c9e5bd8c94a568db1f4
Jake Miller <jake0miller@gmail.com>
2019-09-16 09:19:29 -0600

    Add gifs to controller, need to make facade, serializer, and servce

--- app/controllers/api/v1/gifs_controller.rb
+++ app/controllers/api/v1/gifs_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::GifsController < ApplicationController
     fcast = Rails.cache.fetch("forecasts/#{location}", expires_in: 1.minutes) do
       ForecastFacade.new(location).forecast
     end
-
-    render json: ForecastSerializer.new(fcast)
+    gifs = GifFacade.new(fcast).gifs
+    render json: GifSerializer.new(gifs)
   end
 end
