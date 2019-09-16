134ad7e43aded5260bdbf558c938605d60741351
Jake Miller <jake0miller@gmail.com>
2019-09-16 10:59:03 -0600

    Force downcase on user emails

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   validates :email, uniqueness: {case_sensitive: false}, presence: true
 
   has_secure_password
+
+  before_save { self.email = self.email.to_s.downcase }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,4 @@ RSpec.describe User, type: :model do
     it {should validate_presence_of :api_key}
     it {should validate_uniqueness_of(:email).case_insensitive}
   end
-
-  describe 'Relationships' do
-
-  end
 end
