d480c523e9c60017ac89fe60b9292667a2df7900
Jake Miller <jake0miller@gmail.com>
2019-09-16 09:38:43 -0600

    Create giphy service

--- app/facades/gif_facade.rb
+++ app/facades/gif_facade.rb
@@ -6,20 +6,20 @@ class GifFacade
   def gifs
     # binding.pry
     @forecast.each do |forecast|
-      forecast[:url] = gif(forecast)
+      forecast[:url] = gif(forecast[:summary])
     end
     @forecast
   end
 
-  def gif(forecast)
-    gif = Gif.find_or_create_by(description: forecast[:summary])
+  def gif(summary)
+    gif = Gif.find_or_create_by(description: summary)
     return gif.url if gif && gif.url
-
+    service(summary)
   end
 
   private
 
   def service(summary)
-    @_service ||= Giphy.new(summary).gif
+    @_service ||= Giphy.new.gif(summary)
   end
 end
