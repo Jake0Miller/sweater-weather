6cc5883fefe1ee2aa797076d1ed765998548b986
Jake Miller <jake0miller@gmail.com>
2019-09-18 11:46:34 -0600

    Fix params in sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SessionsController < ApplicationController
   def new
     user = User.find_by(email: params[:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:password])
       render json: {api_key: user.api_key}
     else
       render json: {error: 'Username/password do not match'}, status: 401
